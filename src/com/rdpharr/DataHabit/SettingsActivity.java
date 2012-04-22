81c0061c7f5787573c1c8141b547c7dc60be9495
rdpharr <rdpharr@gmail.com>
2012-04-22 08:45:04 -1000

    Fix for occasional crash due to log.d

--- src/com/rdpharr/DataHabit/SettingsActivity.java
+++ src/com/rdpharr/DataHabit/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
