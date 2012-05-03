668adeb4be54289b54e2dc3cf254ab0500e79e90
rdpharr <rdpharr@gmail.com>
2012-05-02 20:22:57 -1000

    javadoc for tracker and dataPoint classes

--- src/com/rdpharr/DataHabit/DataPoint.java
+++ src/com/rdpharr/DataHabit/DataPoint.java
@@ -3,7 +3,11 @@ package com.rdpharr.DataHabit;
 import org.joda.time.DateTime;
 import android.content.Context;
 import android.database.Cursor;
-
+/**
+ * Class that records the values, comments, 
+ * and times for each data point
+ * @author roger pharr
+ */
 public class DataPoint {
 	private int id; //autonumbered by sqlite, 0 means new tracker
 	private int trackerId;//id of parent tracker
@@ -12,13 +16,23 @@ public class DataPoint {
 	private String comment;//user entered comment
 	private long updateTime; //system time of save
 	private dbAdapter mDbHelper;
-	
+	/**
+	 * Class that records the values, comments, 
+	 * and times for each data point
+	 * @param ctx			Activity Context
+	 * @param trackerId		Unique ID for tracker (parent)
+	 * @param rowId			Unique ID for data point
+	 */
 	public DataPoint(Context ctx, int trackerId, int rowId){
 		this.trackerId = trackerId;
 		id=rowId;//id will be 0 if new
 		if (id ==0) setDefaults(ctx);
 		else getFromDb(ctx);
 	}
+	/**
+	 * submits data point to database
+	 * @param ctx	Activity Context
+	 */
 	public void submitData(Context ctx){
 		mDbHelper = new dbAdapter(ctx);
         mDbHelper.open();
@@ -44,6 +58,11 @@ public class DataPoint {
 		}
 		mDbHelper.close();
 	}
+	/**
+	 * Sets default values for data point
+	 * value=0, date/time=now, comment=""
+	 * @param ctx	Activity Context
+	 */
 	private void setDefaults(Context ctx){
 		DateTime dt = new DateTime();
 		time=dt.getMillis();
@@ -51,6 +70,10 @@ public class DataPoint {
 		comment="";
 		updateTime=dt.getMillis();
 	}
+	/** 
+	 * Gets values for data point form database
+	 * @param ctx	Activity Context
+	 */
 	private void getFromDb(Context ctx){
 		mDbHelper = new dbAdapter(ctx);
         mDbHelper.open();
@@ -60,41 +83,80 @@ public class DataPoint {
         comment=c.getString(4);
         updateTime=c.getLong(5);
     }
-	
-	//getters/setters
+	/**
+	 * gets unique id for data point
+	 * @return unique id for data point
+	 */
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	/**
+	 * gets unique id of Tracker associated with this data point
+	 * @return tracker id
+	 */
 	public int getTrackerId() {
 		return trackerId;
 	}
+	/**
+	 * sets unique id of tracker associated with this data point
+	 * @param trackerId unique id of tracker
+	 */
 	public void setTrackerId(int trackerId) {
 		this.trackerId = trackerId;
 	}
+	/**
+	 * gets time displayed for data point
+	 * @return time in millis
+	 */
 	public long getTime() {
 		return time;
 	}
+	/**
+	 * sets time associated with the data point
+	 * @param time time in millis
+	 */
 	public void setTime(long time) {
 		this.time = time;
 	}
+	/**
+	 * gets user entered value/number associated with data point
+	 * @return value
+	 */
 	public double getValue() {
 		return value;
 	}
+	/**
+	 * sets user entered value/number associated with this data point
+	 * @param value 
+	 */
 	public void setValue(double value) {
 		this.value = value;
 	}
+	/**
+	 * user entered comment for the data point
+	 * @return comment
+	 */
 	public String getComment() {
 		return comment;
 	}
+	/**
+	 * user entered comment for the data point
+	 * @param comment
+	 */
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
+	/**
+	 * system generated time data was set in database
+	 * @return time in millis
+	 */
 	public long getUpdateTime() {
 		return updateTime;
 	}
+	/**
+	 * system generated time data was set in database
+	 * @param updateTime time in millis
+	 */
 	public void setUpdateTime(long updateTime) {
 		this.updateTime = updateTime;
 	}
--- src/com/rdpharr/DataHabit/Tracker.java
+++ src/com/rdpharr/DataHabit/Tracker.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 
 
-/*
- *Main class for accessing Datahabit trackers 
+/**Main class for accessing datahabit trackers
+ *@author roger pharr
  */
 
 public class Tracker {
@@ -19,11 +19,20 @@ public class Tracker {
 	private long nextReminder; //timestamp
 	private long lastUpdate; //timestamp
 	
+	/**Tracker is the object that stores name, type, and reminder info for the items tracked
+	 * @param ctx 			Activity Context
+	 * @param trackerId		Unique ID of tracker
+	 */
 	public Tracker(Context ctx, int trackerId){
 		//trackerId will be 0 if new
 		if (trackerId ==0) setTrackerDefaults(ctx);
 		else getTrackerFromDb(ctx,trackerId);
 	}
+	/**Defaults for Tracker:
+	 * id=0, name="click to set title", reminders=0
+	 * reminder freq=1, cycle=days (1), date=now
+	 * @param ctx			Activity Context
+	 */
 	private void setTrackerDefaults(Context ctx){
 		id=0;
 		name=ctx.getString(R.string.set_title);//default title "Click to set title"
@@ -35,6 +44,11 @@ public class Tracker {
 		nextReminder=dt.getMillis(); //now
 		lastUpdate=dt.getMillis();//now
 	}
+	/**
+	 * Gets tracker data from database
+	 * @param ctx 			Activity Context
+	 * @param trackerId		Unique ID of tracker
+	 */
 	private void getTrackerFromDb(Context ctx, int TrackerId){
 		
 		//open the database and fetch the values
@@ -56,6 +70,10 @@ public class Tracker {
 		c.close();
 		mDbHelper.close();
 	}
+	/**
+	 * Submits data to database
+	 * @param ctx			Activity Context
+	 */
 	public void submitData(Context ctx){
 		dbAdapter mDbHelper=new dbAdapter(ctx);
         mDbHelper.open();
@@ -65,6 +83,10 @@ public class Tracker {
 				reminderFreq, reminderCycle, nextReminder, lastUpdate);
 		mDbHelper.close();
 	}
+	/**
+	 * Deletes tracker form database
+	 * @param ctx			Activity Context
+	 */
 	public void delete(Context ctx){
 		//open the database and fetch the values
 		dbAdapter mDbHelper= new dbAdapter(ctx);
@@ -89,67 +111,111 @@ public class Tracker {
 	
 	
 	//just getters/setters down here
-	
+	/**
+	 * Unique ID of tracker
+	 * @return Unique id of tracker
+	 */
 	public int getId() {
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	/**
+	 * Name of tracker
+	 * @return Name that the user set for the tracker
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * Sets name of tracker
+	 * @param name	Name of Tracker
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * Type of tracker (star rating, numeric data, etc.
+	 * @return 	index of "inut_types" array
+	 */
 	public int getType() {
 		return type;
 	}
-
+	/**
+	 * Type of tracker (star rating, numeric data, etc.
+	 * @param type	index of "imput_types" array
+	 */
 	public void setType(int type) {
 		this.type = type;
 	}
-
+	/**
+	 * whether reminders are enabled for the tracker
+	 * @return 0 for false, 1 for true
+	 */
 	public int getReminderEnabled() {
 		return reminderEnabled;
 	}
-
+	/**
+	 * whether reminders are enabled for the tracker
+	 * @param reminderEnabled	0 for false, 1 for true
+	 */
 	public void setReminderEnabled(int reminderEnabled) {
 		this.reminderEnabled = reminderEnabled;
 	}
-
+	/**
+	 * Times per cycle that reminders should occur. The "2" in
+	 * "2 times per day"
+	 * @return			number of times per cycle
+	 */
 	public int getReminderFreq() {
 		return reminderFreq;
 	}
-
+	/**
+	 * Times per cycle that reminders should occur. The "2" in
+	 * "2 times per day"
+	 * @param reminderFreq	number of times per cycle
+	 */
 	public void setReminderFreq(int reminderFreq) {
 		this.reminderFreq = reminderFreq;
 	}
-
+	/**
+	 * The cycle of reminders (hours,days,weeks) 
+	 * @return index of "reminder_frequencies" array
+	 */
 	public int getReminderCycle() {
 		return reminderCycle;
 	}
-
+	/**
+	 * The cycle of reminders (hours,days,weeks)
+	 * @param reminderCycle index of "reminder_frequencies" array
+	 */
 	public void setReminderCycle(int reminderCycle) {
 		this.reminderCycle = reminderCycle;
 	}
-
+	/**
+	 * the time of next reminder
+	 * @return time in millis
+	 */
 	public long getNextReminder() {
 		return nextReminder;
 	}
-
+	/**
+	 * timme of next reminder
+	 * @param nextReminder	time in millis
+	 */
 	public void setNextReminder(long nextReminder) {
 		this.nextReminder = nextReminder;
 	}
-
+	/**
+	 * time of last update
+	 * @return time in millis
+	 */
 	public long getLastUpdate() {
 		return lastUpdate;
 	}
-
+	/**
+	 * time of last update
+	 * @param lastUpdate	time in millis
+	 */
 	public void setLastUpdate(long lastUpdate) {
 		this.lastUpdate = lastUpdate;
 	}
